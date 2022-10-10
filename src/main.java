20f5dbc64a9832f811e128ae0e24212a80920c7a
darek <dczajka@tuta.io>
2022-10-10 18:56:44 +0200

    commit message

---
+++ src/main.java
@@ -0,0 +1,32 @@
+public class main {
+    public static void main(String[] args) {
+
+        // while
+/*
+        int liczba = 0;
+
+        while (liczba > 10) {
+            System.out.println(liczba);
+            liczba++;
+        }
+
+        // do-while
+
+        int liczba = 0;
+        do{
+            System.out.println(liczba);
+            liczba++;
+        }while(liczba > 10);
+
+*/
+
+        // for
+
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
+
+    }
+}
+
